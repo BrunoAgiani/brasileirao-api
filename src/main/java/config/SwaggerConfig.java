4f7b4262b94c191718c815bb30749d73cdb317a3
BrunoAgiani <brunoaalisboa@gmail.com>
2024-02-29 19:08:23 -0300

    criacao de beans modelmapper e swagger

---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "controller";
+    private static final String API_TITULO = "Brasileirao API - Scraping";
+    private static final String API_DESCRICAO = "API REST que obtem dados de partidas";
+    private static final String API_VERSAO = "1.0.0";
+    private static final String CONTATO_NOME = "Bruno Agiani";
+    private static final String CONTATO_GITHUB = "https://github.com/BrunoAgiani";
+    private static final String CONTATO_EMAIL = "brunoaalisboa@gmail.com";
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE)).paths(PathSelectors.any()).build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITULO).description(API_DESCRICAO)
+                .version(API_VERSAO).contact(new Contact(CONTATO_NOME,CONTATO_GITHUB,CONTATO_EMAIL))
+                .build();
+    }
+}
